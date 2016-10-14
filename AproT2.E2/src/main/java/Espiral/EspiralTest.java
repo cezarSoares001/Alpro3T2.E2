7e6dedff28085bc2a32ecc1cd6175fcea1a5793e
cezarSoares001 <cezar.oliveira@acad.pucrs.br>
2016-10-14 14:04:41 -0300

    Add files via upload

---
+++ AproT2.E2/src/main/java/Espiral/EspiralTest.java
@@ -0,0 +1,105 @@
+package Espiral;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EspiralTest {
+
+	
+	@Test(expected = IllegalArgumentException.class)
+    public void testInvalidNumber() {
+		int actual = Tabuleiro.entradaT(-1,0);
+    }
+	
+	@Test
+	public void testCase1() {
+		int[] expected = {1,3};
+		int actual = Tabuleiro.entradaT(3,3);
+		
+		assertEquals(expected, actual);
+		
+	}
+	
+	@Test
+	public void testCase2() {
+		int[] expected = {2,2};
+		int actual = Tabuleiro.entradaT(3,9);
+		
+		assertEquals(expected, actual);
+		
+	}
+	
+	@Test
+	public void testCase3() {
+		int[] expected = {3,1};
+		int actual = Tabuleiro.entradaT(3,7);
+		
+		assertEquals(expected, actual);
+		
+	}
+	
+	@Test
+	public void testCase4() {
+		int[] expected = {3,3};
+		int actual = Tabuleiro.entradaT(3,5);
+		
+		assertEquals(expected, actual);
+		
+	}
+	
+	@Test
+	public void testCase5() {
+		int[] expected = {2,1};
+		int actual = Tabuleiro.entradaT(3,8);
+		
+		assertEquals(expected, actual);
+		
+	}
+	
+	@Test
+	public void testCase6() {
+		int[] expected = {1,1};
+		int actual = Tabuleiro.entradaT(4,1);
+		
+		assertEquals(expected, actual);
+		
+	}
+	
+	@Test
+	public void testCase7() {
+		int[] expected = {1,1};
+		int actual = Tabuleiro.entradaT(10,1);
+		
+		assertEquals(expected, actual);
+		
+	}
+	
+	@Test
+	public void testCase8() {
+		int[] expected = {4,1};
+		int actual = Tabuleiro.entradaT(4,10);
+		
+		assertEquals(expected, actual);
+		
+	}
+	
+	@Test
+	public void testCase9() {
+		int[] expected = {10,9};
+		int actual = Tabuleiro.entradaT(10,20);
+		
+		assertEquals(expected, actual);
+		
+	}
+	
+	@Test
+	public void testCaseEspiral() {
+		int[] expected = {4,6};
+		int actual = Tabuleiro.entradaT(8,53);
+		
+		assertEquals(expected, actual);
+		
+	}
+
+}
---
+++ AproT2.E2/src/main/java/Espiral/Tabuleiro.java
@@ -0,0 +1,14 @@
+package Espiral;
+
+public class Tabuleiro {
+
+	public static int entradaT(int n, int b){
+		if(n<=0 || b<=0){
+			throw new IllegalArgumentException();
+		}
+		return 0;
+	}
+
+	
+	
+}
